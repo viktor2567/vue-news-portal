15b3c9a9fe6451fc9b959cb0d33a263a81948de8
viktor2567 <nevmerzhitskiy.2016@gmail.com>
2022-11-06 11:50:22 +0200

    (feat) add server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('experss')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+
+const app = express()
+
+
+app.use('/', serveStatic(path.join(__dirname, 'dist')))
+
+app.get(/.*/,function(req, res){
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
+
+const port = process.evn.PORT || 8080
+app.listen(port)
+console.log(`app listen port: ${port}`)
