9e7712b7a93a6dad4d1558a576a99d85ee19fe0f
viktor2567 <nevmerzhitskiy.2016@gmail.com>
2022-11-06 12:54:03 +0200

    (fix) server->PORT

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get(/.*/,function(req, res){
     res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
-const port = process.evn.PORT || 8080
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app listen port: ${port}`)
