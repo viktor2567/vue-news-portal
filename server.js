a3372c8943c2234f22cc8fef52e2c96c38d22971
viktor2567 <nevmerzhitskiy.2016@gmail.com>
2022-11-06 13:18:51 +0200

    server json

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({
+  static: './build'
+});
+const PORT = process.env.PORT || 8000;
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
+server.use(router);
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
 
-
-const app = express()
-
-
-app.use('/', serveStatic(path.join(__dirname, 'dist')))
-
-app.get(/.*/,function(req, res){
-    res.sendFile(path.join(__dirname, 'dist/index.html'))
-})
-
-const port = process.env.PORT || 8080
-app.listen(port)
-console.log(`app listen port: ${port}`)
