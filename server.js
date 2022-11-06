9c10649e921ae4cf85b6fbd96031c361a7bdd849
viktor2567 <nevmerzhitskiy.2016@gmail.com>
2022-11-06 13:36:51 +0200

    (fix) post.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
-});
-const PORT = process.env.PORT || 8000;
-server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}))
-server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
-});
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
+
+const app = express()
+
+
+app.use('/', serveStatic(path.join(__dirname, 'dist')))
+
+app.get(/.*/,function(req, res){
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
+
+const port = process.env.PORT || 9000
+app.listen(port)
+console.log(`app listen port: ${port}`)
--- src/store/modules/post.js
+++ src/store/modules/post.js
@@ -3,21 +3,21 @@ import Axios from "axios";
 export default {
   actions: {
     async fetchPosts({commit}) {
-      const response = await Axios.get('http://localhost:3000/posts?_sort=id&_order=DESC');
+      const response = await Axios.get('https://whispering-savannah-85934.herokuapp.com/posts?_sort=id&_order=DESC');
 		commit('updatePosts', response.data);
 		console.log('data', response.data);
     }, 
     async newPost({commit}, post) {
-      await Axios.post('http://localhost:3000/posts', post);
+      await Axios.post('https://whispering-savannah-85934.herokuapp.com/posts', post);
       commit('NEW_POSTS', post)
     },
     async idCard({ commit }, id) {
-      const response = await Axios.get(`http://localhost:3000/posts/${id}`);
+      const response = await Axios.get(`https://whispering-savannah-85934.herokuapp.com/posts/${id}`);
       commit('updateCard', response);
 		console.log('data', response);},
 
     async Delete({ commit }, id) {
-      const response = await Axios.delete(`http://localhost:3000/posts/${id}`);
+      const response = await Axios.delete(`https://whispering-savannah-85934.herokuapp.com/posts/${id}`);
       commit('Delete', response);
 		console.log('data', response);
   },},
